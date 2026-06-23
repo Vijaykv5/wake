e355bd0cf8d626a3423035c5b4357ad80823c96d
vijaykv5 <vijaykv2228@gmail.com>
2026-06-23 17:29:06 +0530

    feat : get_battery_info function implementation

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,33 +1,39 @@
+#[derive(serde::Serialize)]
+struct BatteryInfo {
+    percentage: f32,
+    charging: bool,
+}
 
-// #[derive(serde::Serialise)]
-// struct BatteryInfo{
-//     percentage : f32,
-//     charging :  bool
-// }
+#[tauri::command]
+fn get_battery_info() -> Result<BatteryInfo, String> {
+    let manager = battery::Manager::new().map_err(|e| e.to_string())?;
 
-// #[tauri::command] //expose this rust function to frontend so that that the react can use it 
-// fn get_battery_info() -> Result<BatteryInfo, String>{
-//     let manager = battery::Manager::new().map_err(|e| e.to_string())?;
+    let mut batteries = manager.batteries().map_err(|e| e.to_string())?;
 
-//     let mut batteries = manager.batteries().map_err(|e| e.to_string())?;
+    if let Some(battery_result) = batteries.next() {
+        let battery = battery_result.map_err(|e| e.to_string())?;
 
-//     if let Some(battery_result)=batteries.next(){
-//         let battery = battery_result.map_err(|e| e.to_string())?;
-//     }
+        let percentage = battery.state_of_charge().value * 100.0;
 
-// }
+        let charging = matches!(
+            battery.state(),
+            battery::State::Charging | battery::State::Full
+        );
 
+        Ok(BatteryInfo {
+            percentage,
+            charging,
+        })
+    } else {
+        Err("No battery found".to_string())
+    }
+}
 
-
-
-
-
-
-// #[cfg_attr(mobile, tauri::mobile_entry_point)]
-// pub fn run() {
-//     tauri::Builder::default()
-//         .plugin(tauri_plugin_opener::init())
-//         .invoke_handler(tauri::generate_handler![greet])
-//         .run(tauri::generate_context!())
-//         .expect("error while running tauri application");
-// }
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![get_battery_info])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
